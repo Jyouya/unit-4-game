df6525201fa9f7e1eb23471ff495c0a22ce323ff
Will West <willwest8xp@gmail.com>
2019-07-19 12:37:42 -0400

    Added character select

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,41 +4,58 @@ const main = {
     initialize() {
         combatants = { // fill combatants with a fresh array of fighters when we initialize
             "Obi Wan": new Fighter("Obi Wan", 120, 8, 20),
-            "Darth Maul": new Fighter("Darth Maul", 189, 5, 25),
+            "Darth Maul": new Fighter("Darth Maul", 180, 5, 25),
             "Luke Skywalker": new Fighter("Luke Skywalker", 100, 10, 5),
             "Darth Sidious": new Fighter("Darth Sidious", 150, 6, 15)
         };
         this.state = 'character select';
-        combatants.forEach(fighter => {
-            
-        });
-    },
-};
 
-{/* <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-</div> */}
+        const stage = $('#stage');
+        for (const name in combatants) {
+            stage.append(makeCard(combatants[name]));
+        }
+
+        $('#game').on('click', '.fighter', cardListener);
+
+        $('#message').text('Choose your fighter');
+    },
+    char: '',
+};
 
 
 function makeCard(fighter) {
-    $('<div>').addClass('card fighter').addValue(fighter.name).appendChild(
-        $('<img>').attr('src', '../images/Obi Wan.png')
-    ).appendChild(
-        $('<div>').addClass('card-body').appendChild(
-            $('<h5>').addClass('card-title').text(fighter.name)
-        ).appendChild(
+    console.log(fighter);
+    return $('<div>').addClass('card fighter p-1 col-6 col-md-3').attr('value', fighter.name).append(
+        $('<img>').attr('src', `assets/images/${fighter.name}.png`),
+        $('<div>').addClass('card-body').append(
+            $('<h5>').addClass('card-title').text(fighter.name),
             $('<p>').addClass('card-text').text(fighter.HP)
         )
     );
 }
 
+function cardListener(event) {
+    switch (main.state) {
+        case 'character select':
+            $(this).detach().removeClass('col-6 col-md-3').appendTo('#your-character');
+            main.state = 'oponent select';
+            char = combatants[event.currentTarget.value];
+            $('#message').text('Choose your opponent');
+            return;
+        case 'oponent select':
+            if (event.currentTarget.value != main.char) {
+                $(this).detach().removeClass('col-6 col-md-3').appendTo('#defender');
+                $('#message').text('Fight!')
+                main.state = 'combat';
+            }
+
+    }
+}
+
 
 $(document).ready(function () {
     main.initialize();
+
+
 
 });
